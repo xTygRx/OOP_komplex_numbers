098dee87e931d337e2aa9750a04e60bde514e413
xtygrx <divinged@inbox.ru>
2024-05-01 08:42:47 +0300

    add commit

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в калькулятор комплексных чисел");
         System.out.println("Введите комплексное число в алгебраической форме");
+        System.out.println("Например 1+ 2i или 3 + i  или если коэффициент мнимой части равен 0, то введите только значимую часть - 3");
 
         InterfaceReliz interfaceReliz = new InterfaceReliz();
         interfaceReliz.choice();
